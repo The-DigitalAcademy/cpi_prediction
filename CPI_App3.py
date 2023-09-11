db6170a9154b000c7d4a31ed162db859d9970b22
Zandile_Mdiniso <132349003+ZandileMdiniso@users.noreply.github.com>
2023-09-11 14:43:31 +0200

    Update CPI_App3.py

--- CPI_App3.py
+++ CPI_App3.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import pandas as pd
 import os
-from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.models import load_model
 import datetime
-from collections import defaultdict
 
 # Define the target columns
 target_cols = ['Headline_CPI', 'Alcoholic beverages and tobacco', 'Clothing and footwear',
@@ -13,21 +11,26 @@ target_cols = ['Headline_CPI', 'Alcoholic beverages and tobacco', 'Clothing and 
               'Housing and utilities', 'Miscellaneous goods and services',
               'Recreation and culture', 'Restaurants and hotels ', 'Transport']
 
-# Create input data for prediction
-def create_input_data(selected_category, input_values, vehicle_sales, currency_input):
+# Function to load models for all categories
+def load_models():
+    loaded_models = {}
+    for column in target_cols:
+        for i in range(1, 4):
+            model_path = os.path.join(f"{column}_Deep Neural Network_month_{i}.h5")
+            if os.path.exists(model_path):
+                loaded_model = load_model(model_path)
+                loaded_models[f"{column}_month_{i}"] = loaded_model
+    return loaded_models
+
+# Function to create input data for prediction
+def create_input_data(selected_category, previous_cpi_value, vehicle_sales, currency_input):
     input_data = pd.DataFrame(columns=target_cols)  # Create an empty DataFrame
-    input_data.at[0, selected_category] = input_values["previous_cpi_value"]
+    input_data.at[0, selected_category] = previous_cpi_value
     input_data.at[0, 'Vehicle Sales'] = vehicle_sales
     input_data.at[0, 'Currency Input'] = currency_input
-
-    # Set other features to 0 as placeholders, you can update this as needed
-    for col in target_cols:
-        if col != selected_category and col not in ['Vehicle Sales', 'Currency Input']:
-            input_data.at[0, col] = 0.0  # You can update this with actual input values
-
     return input_data
 
-# Make predictions for a selected category
+# Function to make predictions for a category
 def make_prediction(selected_category, input_data, loaded_models, category_formatted, predictions, reference_date):
     for i in range(1, 4):
         model_key = f"{selected_category}_month_{i}"
@@ -49,21 +52,18 @@ def main():
     previous_cpi_value = st.number_input(f"Previous CPI for {selected_category}", value=0.0)
 
     # Display input fields for vehicle sales and currency
+    st.write("Enter Vehicle Sales and Currency Input:")
     vehicle_sales = st.number_input("Vehicle Sales", value=0.0)
     currency_input = st.number_input("Currency Input", value=0.0)
 
-    # Store the input values for the selected category
-    input_values = {
-        "previous_cpi_value": previous_cpi_value,
-        "vehicle_sales": vehicle_sales,
-        "currency_input": currency_input
-    }
+    # Load saved models
+    loaded_models = load_models()
+
+    # Create input data for prediction
+    input_data = create_input_data(selected_category, previous_cpi_value, vehicle_sales, currency_input)
 
     # Add a button to trigger model predictions
     if st.button("Predict CPI"):
-        # Load models and make predictions for the selected category
-        loaded_models = load_models(target_cols)  # Load your models here
-
         # Dictionary to store predictions
         predictions = {}
 
@@ -80,12 +80,11 @@ def main():
             reference_date = current_date.replace(month=current_date.month + 3)
 
         # Make predictions for the selected category
-        input_data = create_input_data(selected_category, input_values, vehicle_sales, currency_input)
-        category_formatted = selected_category.replace(' ', '_')  # Replace spaces with underscores
-        make_prediction(selected_category, input_data, loaded_models, category_formatted, predictions, reference_date)
+        make_prediction(selected_category, input_data, loaded_models, selected_category.replace(' ', '_'), predictions, reference_date)
 
         # Display predictions
         st.write(f"Predicted CPI values for {selected_month} for {selected_category}:")
+        category_formatted = selected_category.replace(' ', '_')  # Replace spaces with underscores
         st.write(f"{selected_category} CPI for {reference_date.strftime('%B_%Y')}: {predictions[category_formatted + '_CPI_for_' + reference_date.strftime('%B_%Y')]:.2f}")
 
 if __name__ == "__main__":
