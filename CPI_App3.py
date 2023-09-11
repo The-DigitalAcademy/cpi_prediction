29f1ba5964ca1b45de0711f93c6725cf4c319b0a
Zandile_Mdiniso <132349003+ZandileMdiniso@users.noreply.github.com>
2023-09-11 14:09:17 +0200

    Update CPI_App3.py

--- CPI_App3.py
+++ CPI_App3.py
@@ -6,6 +6,7 @@ from tensorflow.keras.models import load_model
 import datetime
 from collections import defaultdict
 
+# Define the target columns
 target_cols = ['Headline_CPI', 'Alcoholic beverages and tobacco', 'Clothing and footwear',
               'Communication', 'Education', 'Food and non-alcoholic beverages',
               'Health', 'Household contents and services',
@@ -18,41 +19,41 @@ def main():
     st.title("CPI Prediction Dashboard")
 
     # Display a dropdown to select the category for prediction
-    selected_category = st.selectbox("Select a category to predict:", target_cols)
+    selected_categories = st.multiselect("Select categories to predict:", target_cols)
 
-    # Display input fields for previous CPI values
-    st.write(f"Enter previous CPI value for {selected_category}:")
-    previous_cpi_value = st.number_input(f"Previous CPI for {selected_category}", value=0.0)
+    if not selected_categories:
+        st.warning("Please select at least one category to predict.")
+        return
 
-    # Display input fields for vehicle sales and currency
-    vehicle_sales = st.number_input("Vehicle Sales", value=0.0)
-    currency_input = st.number_input("Currency Input", value=0.0)
+    # Create a dictionary to store input values for each category
+    input_values = {}
 
-    # Dictionary to store loaded models
-    loaded_models = {}
+    # Iterate over selected categories
+    for selected_category in selected_categories:
+        # Display input fields for previous CPI values
+        st.write(f"Enter previous CPI value for {selected_category}:")
+        previous_cpi_value = st.number_input(f"Previous CPI for {selected_category}", value=0.0)
 
-    # Iterate over target columns and months
-    for column in target_cols:
-        for i in range(1, 4):
-            model_path = os.path.join(f"{column}_Deep Neural Network_month_{i}.h5")
-            if os.path.exists(model_path):
-                loaded_model = load_model(model_path)
-                loaded_models[f"{column}_month_{i}"] = loaded_model
+        # Display input fields for vehicle sales and currency
+        vehicle_sales = st.number_input(f"{selected_category} Vehicle Sales", value=0.0)
+        currency_input = st.number_input(f"{selected_category} Currency Input", value=0.0)
 
-    # Create input data for prediction
-    input_data = pd.DataFrame(columns=target_cols)  # Create an empty DataFrame
-    input_data.at[0, selected_category] = previous_cpi_value
-    input_data.at[0, 'Vehicle Sales'] = vehicle_sales
-    input_data.at[0, 'Currency Input'] = currency_input
+        # Store the input values for the selected category
+        input_values[selected_category] = {
+            "previous_cpi_value": previous_cpi_value,
+            "vehicle_sales": vehicle_sales,
+            "currency_input": currency_input
+        }
 
-    # Display input fields for other X_train columns
-    st.write("Enter previous values for other features:")
-    for col in target_cols:
-        if col != selected_category and col not in ['Vehicle Sales', 'Currency Input']:
-            input_value = st.number_input(f"Previous {col}", value=0.0)
-            input_data.at[0, col] = input_value
+    # Add a button to trigger model predictions
+    if st.button("Predict CPI"):
+        # Load models and make predictions for each category
+        loaded_models = load_models(target_cols)  # Load your models here
 
-           # Allow the user to select which month they want to predict
+        # Dictionary to store predictions
+        predictions = {}
+
+        # Allow the user to select which month they want to predict
         selected_month = st.selectbox("Select a month for prediction:", ["Next Month", "Two Months Later", "Three Months Later"])
 
         # Calculate the reference date based on the current date
@@ -64,23 +65,17 @@ def main():
         elif selected_month == "Three Months Later":
             reference_date = current_date.replace(month=current_date.month + 3)
 
-        # Dictionary to store predictions
-        predictions = {}
-
-        # Iterate over target columns and months
-        for category in target_cols:
-            for i in range(1, 4):
-                model_key = f"{category}_month_{i}"
-                if model_key in loaded_models:
-                    loaded_model = loaded_models[model_key]
-                    y_pred = loaded_model.predict(input_data)
-                    category_formatted = category.replace(' ', '_')  # Replace spaces with underscores
-                    predictions[f'{category_formatted}_CPI_for_{reference_date.strftime("%B_%Y")}'] = round(y_pred[0][0], 2)
+        # Iterate over selected categories and make predictions
+        for selected_category in selected_categories:
+            input_data = create_input_data(selected_category, input_values[selected_category])
+            category_formatted = selected_category.replace(' ', '_')  # Replace spaces with underscores
+            make_prediction(selected_category, input_data, loaded_models, category_formatted, predictions, reference_date)
 
         # Display predictions
-        st.write(f"Predicted CPI values for {selected_month} for {selected_category}:")
-        category_formatted = selected_category.replace(' ', '_')  # Replace spaces with underscores
-        st.write(f"{selected_category} CPI for {reference_date.strftime('%B_%Y')}: {predictions[category_formatted + '_CPI_for_' + reference_date.strftime('%B_%Y')]:.2f}")
+        st.write(f"Predicted CPI values for {selected_month}:")
+        for selected_category in selected_categories:
+            category_formatted = selected_category.replace(' ', '_')  # Replace spaces with underscores
+            st.write(f"{selected_category} CPI for {reference_date.strftime('%B_%Y')}: {predictions[category_formatted + '_CPI_for_' + reference_date.strftime('%B_%Y')]:.2f}")
 
 if __name__ == "__main__":
     main()
