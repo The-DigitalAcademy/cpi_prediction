bc0d5e3ea350e94f0611016a628aaa7cb731295e
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-09-12 13:44:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 import streamlit as st
 import pandas as pd
-import joblib
-from sklearn.preprocessing import MinMaxScaler
-import streamlit as st
-import pandas as pd
 import numpy as np
 import joblib
-
-# Load your trained models
-#loaded_models = {}
-target_cols = ['Alcoholic beverages and tobacco', 'Clothing and footwear',
-               'Communication', 'Education', 'Food and non-alcoholic beverages',
-               'Headline_CPI', 'Health', 'Household contents and services',
-               'Housing and utilities', 'Miscellaneous goods and services',
-               'Recreation and culture', 'Restaurants and hotels ', 'Transport']
-
 
 # Load the saved models
 model_filenames = {
@@ -38,6 +25,7 @@ lr_models = {}
 for col, model_filename in model_filenames.items():
     lr_models[col] = joblib.load(model_filename)
 
+# Create the Streamlit app
 st.title('CPI Prediction for April')
 
 # Input features
@@ -49,136 +37,32 @@ selected_category = st.selectbox('Select Category', list(model_filenames.keys())
 # Select month
 selected_month = st.text_input('Enter Month (YYYY-MM)', '2023-03')  # Default value for example
 
-import streamlit as st
-import pickle
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.linear_model import LinearRegression
+# Define a function to make predictions
+@st.cache
+def make_prediction(category, month):
+    # Assuming you have a function or code to load your dataset and preprocess it
+    # Replace this with your actual dataset loading and preprocessing code
+    # dataset = load_and_preprocess_dataset()
 
-# Load the linear regression models
-model_filenames = {
-    'Alcoholic beverages and tobacco': 'model_alcoholic_tobacco.pkl',
-    'Clothing and footwear': 'model_clothing_footwear.pkl',
-    # Add model filenames for other columns...
-}
+    # Assuming you have a function or code to extract the lagged feature value
+    # Replace this with your actual feature extraction code
+    # lagged_feature = extract_lagged_feature(dataset, selected_category, selected_month)
 
-lr_models = {}
-for col, model_filename in model_filenames.items():
-    with open(model_filename, 'rb') as model_file:
-        lr_models[col] = pickle.load(model_file)
+    # Instead of the above lines, you can manually set a default value for the lagged feature
+    # Here, we assume a default value of 0.0 for the example
+    lagged_feature = 0.0
 
-st.title('CPI Prediction for April')
+    # Use the selected model to make predictions
+    lr_model = lr_models[category]
+    user_inputs_scaled = scaler.transform([[lagged_feature]])
+    predicted_cpi = lr_model.predict(user_inputs_scaled)[0]
 
-# Input features
-st.header('Input Features')
-
-# Select category
-selected_category = st.selectbox('Select Category', list(model_filenames.keys()))
-
-# Select month
-selected_month = st.text_input('Enter Month (YYYY-MM)', '2023-03')  # Default value for example
+    return predicted_cpi
 
 # Make predictions
 if st.button('Predict'):
-    # Load the dataset for the selected month
-    dataset_filename = 'your_dataset.csv'  # Replace with your dataset filename
-    dataset = pd.read_csv(dataset_filename)
-
-    # Filter the dataset for the selected month
-    selected_row = dataset[dataset['Month'] == selected_month]
-
-    if not selected_row.empty:
-        # Extract the lagged feature value
-        lagged_feature_value = selected_row[selected_category].values[0]
-
-        # Construct the user_inputs dictionary for making predictions
-        user_inputs = {
-            'prev_1_month_' + selected_category: lagged_feature_value
-        }
-
-        # Scale user inputs
-        user_inputs_scaled = scaler.transform([list(user_inputs.values())])
-
-        # Make predictions
-        lr_model = lr_models[selected_category]
-        predicted_cpi = lr_model.predict(user_inputs_scaled)[0]
-
+    if selected_month and selected_category:
+        predicted_cpi = make_prediction(selected_category, selected_month)
         st.success(f'Predicted CPI for {selected_category} in {selected_month}: {predicted_cpi:.2f}')
     else:
-        st.error(f'Data not available for {selected_month}. Please select a different month.')
-
-
-# Load your dataset here
-# dataset_filename = 'your_dataset.csv'
-# dataset = pd.read_csv(dataset_filename)
-
-# Assuming you have the dataset loaded and preprocessed, you can extract the lagged feature value as follows
-# Replace this part with your actual data preprocessing code
-# You may need to modify this code depending on your dataset structure
-# if selected_month and selected_category:
-#     # Extract the lagged feature value from your dataset
-#     # Assuming your dataset has the necessary columns, adapt this part to your dataset structure
-#     # You may need to modify this code depending on your dataset structure
-#     # selected_row = dataset[(dataset['Month'] == selected_month)]
-#     # lagged_feature = selected_row[f'prev_1_month_{selected_category}'].values[0]
-
-#     # Instead of the above lines, you can manually set a default value for the lagged feature
-#     # Here, we assume a default value of 0.0 for the example
-#     lagged_feature = 0.0
-
-#     # Use the selected model to make predictions
-#     lr_model = lr_models[selected_category]
-#     user_inputs_scaled = scaler.transform([[lagged_feature]])
-#     predicted_cpi = lr_model.predict(user_inputs_scaled)[0]
-
-#     st.success(f'Predicted CPI for {selected_category} in {selected_month}: {predicted_cpi:.2f}')
-
-
-# for target_col in target_cols:
-#     loaded_models[target_col] = joblib.load(f"{target_col}_model.pkl")
-
-# # Load your trained scaler
-# scaler = joblib.load("scaler.pkl")
-
-# # Create Streamlit app
-# st.title("CPI Prediction App")
-
-# # User-friendly input fields
-# selected_category = st.selectbox("Select Category", target_cols)
-# selected_month = st.slider("Select Month", min_value=1, max_value=12)
-# selected_year = st.slider("Select Year", min_value= 2022, max_value= 2023)  # Define min_year and max_year
-
-# # Define a function to make predictions based on user input
-# @st.cache  # Caching the function for improved performance
-# def make_prediction(category, month, year):
-#     # Extract the corresponding model for the selected category
-#     lr_model = loaded_models[category]
-    
-#     # Prepare user input data
-#     user_input = pd.DataFrame({'Month': [f'{year}-{month:02d}-30'], 'Category': [category]})
-    
-#     # Data preprocessing
-#     # Date feature handling
-#     user_input['Month'] = pd.to_datetime(user_input['Month'])
-#     user_input['Year'] = user_input['Month'].dt.year
-#     user_input['Month'] = user_input['Month'].dt.month
-    
-#     # Categorical encoding (assuming one-hot encoding)
-#     user_input = pd.get_dummies(user_input, columns=['Category'], drop_first=True)
-    
-#     # Scaling (assuming Min-Max scaling with the same scaler used during training)
-#     user_input_scaled = scaler.transform(user_input)
-    
-#     # Make predictions using the loaded model
-#     prediction = lr_model.predict(user_input_scaled)
-    
-#     return prediction[0]  # Return the first element of the prediction (assuming it's a single value)
-
-
-# # Display the prediction
-# if st.button("Predict CPI"):
-#     prediction = make_prediction(selected_category, selected_month, selected_year)
-#     st.write(f"Predicted CPI for {selected_category} in {selected_year}-{selected_month:02d} is {prediction:.2f}")
-
-# Optionally, display other information or visualizations
+        st.warning('Please select a category and enter a valid month.')
