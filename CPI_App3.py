54419ec33a42bbac629d3c894789c631199b8c52
Zandile_Mdiniso <132349003+ZandileMdiniso@users.noreply.github.com>
2023-09-12 11:11:59 +0200

    Update CPI_App3.py

--- CPI_App3.py
+++ CPI_App3.py
@@ -95,15 +95,15 @@ def main():
         make_prediction(selected_categories, create_input_data(selected_categories, previous_cpi_value, total_local_sales, total_export_sales, usd_zar, gbp_zar, eur_zar), loaded_models, "_".join(selected_categories), predictions, reference_date, selected_month)
 
         ## Display predictions
-st.write(f"Predicted CPI values for {selected_month} for the selected categories:")
-for category in selected_categories:
-    category_formatted = category.replace(' ', '_')  # Replace spaces with underscores
-    key = f"{category_formatted}_CPI_for_{reference_date.strftime('%B_%Y')}_{selected_month}"
+       st.write(f"Predicted CPI values for {selected_month} for the selected categories:")
+       for category in selected_categories:
+           category_formatted = category.replace(' ', '_')  # Replace spaces with underscores
+           key = f"{category_formatted}_CPI_for_{reference_date.strftime('%B_%Y')}_{selected_month}"
     
-    if key in predictions:
-        st.write(f"{category} CPI for {reference_date.strftime('%B_%Y')}: {predictions[key]:.2f}")
-    else:
-        st.write(f"No prediction found for {category} in {selected_month}")
+           if key in predictions:
+               st.write(f"{category} CPI for {reference_date.strftime('%B_%Y')}: {predictions[key]:.2f}")
+           else:
+               st.write(f"No prediction found for {category} in {selected_month}")
 
 
 if __name__ == "__main__":
