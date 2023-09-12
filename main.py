4ffc44825cd9ee2adefdfe0f7ec1ed784c18f819
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-09-12 14:41:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,20 @@ cpi_pivot = pd.read_csv('UI2.csv')
 
 # Define functions for data preprocessing and model prediction
 def preprocess_data(data):
+    # Convert 'Month' column to datetime format
+    data['Month'] = pd.to_datetime(data['Month'], format='%Y-%m-%d')
+
+    # Add your data preprocessing steps here
+    # For example, fill missing values, handle categorical variables, etc.
+    # You can also include the lagging method you mentioned in your original code here.
+
+    # Assuming you have already created lag features for each category, e.g., 'prev_1_month_...'
+    # Drop unnecessary columns
+    cols_to_drop = ['year_month', 'Total_Local Sales', 'Total_Export_Sales']
+    data = data.drop(columns=cols_to_drop, axis=1)
+
+    # Fill any remaining missing values
+    data = data.fillna(method='bfill')  # Backfill missing value
     # Add your data preprocessing steps here
     return data
 
