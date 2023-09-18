25cfbd00ef34dfc0132608c67b81628beddc0c06
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-09-18 14:32:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
+import matplotlib 
 from sklearn.preprocessing import MinMaxScaler
 
 import matplotlib.pyplot as plt  # Import matplotlib for plotting
