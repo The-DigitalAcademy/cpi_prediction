1992b19a3c8c8936f969db37bfde7bac9b604aaf
Lereko Qholosha <lerekoqholosha9@gmail.com>
2023-09-23 20:47:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -63,21 +63,23 @@ input_data = pd.DataFrame(columns=['year_month', 'Month'])
 data_list = []
 
 # Your code for date range iteration and appending data
+# Initialize an empty DataFrame with columns
+input_data = pd.DataFrame(columns=['year_month', 'Month'])
+
+# Your code for date range iteration and appending data
 while current_date <= end_date:
     year_month = current_date.strftime('%Y-%m')
     month = current_date.strftime('%Y-%m-%d')
 
-    # Create a new row as a dictionary
-    new_row = {'year_month': year_month, 'Month': month}
+    # Create a new row as a Series
+    new_row = pd.Series({'year_month': year_month, 'Month': month})
 
-    # Append the new row to the data_list
-    data_list.append(new_row)
+    # Append the new row to the input_data DataFrame
+    input_data = input_data.append(new_row, ignore_index=True)
 
     # Increment current_date by one month
     current_date = current_date + pd.DateOffset(months=1)
 
-# Create a DataFrame from the data_list
-input_data = pd.DataFrame(data_list)
 
 
 # Load your CPI dataset (replace 'Book6.csv' with your dataset file path)
